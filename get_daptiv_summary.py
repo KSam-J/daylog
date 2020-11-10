4c5db4650121a31da8792e5e12d69ebf32874187
Kellen J. Sam <kellonasam@gmail.com>
2020-11-09 23:58:08 -0700

    Add argparse engine and verbose flag

--- get_daptiv_summary.py
+++ get_daptiv_summary.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 """Read Time log and print summary on stdout."""
+import argparse
 import datetime as dt
 import os
 import re
@@ -9,26 +11,6 @@ import fire
 LOG_PATH = '/home/samkel/journal/oct_time_sheet/'
 
 PRINT_DESCRIPTION = 0
-
-# def printSummary(day, month=dt.date.today().month, filename=''):
-#     with file(f'{LOG_PATH}log{month}_{day}.txt') as log:
-#         for line in log:
-#             hour_search = re.search(
-#                 r'(\d{1,2}):?(\d{1,2})?-(\d{1,2}):?(\d{1,2})?', line)
-#             if hour_search:
-#                 hour1 = hour_search.group(0)
-#                 min1 = 0
-#                 if hour_search.group(1):
-#                     min1 = hour_search.group(1)
-#                 hour2 = hour_search.group(3)
-#                 min2 = 0
-#                 if hour_search.group(4):
-#                     min2 = hour_search.group(4)
-
-#                 start_time = dt.time(hour1, min1)
-#                 end_time
-
-#     return
 
 
 def get_total_hours(month, day, filename=None):
@@ -65,13 +47,14 @@ def get_total_hours(month, day, filename=None):
                 # Calculate and print Time Delta
                 tdelta = end_time - start_time
                 if tdelta.days < 0:
+                    # Make all timedeltas be < 12 hours.
                     tdelta = dt.timedelta(
                         days=0, seconds=(tdelta.seconds - 60*60*12))
                 print(f'{hour1:02}:{min1:02}-{hour2:02}:{min2:02}\t\t\u0394{tdelta}')
 
                 # Accumulate the total hours
                 total += tdelta
-            elif PRINT_DESCRIPTION:  # TODO Turn this into a parser flag
+            elif args.verbose >= 1:
                 # Print None Time delta lines
                 print(line.rstrip())
         # Convert seconds to hours
@@ -81,11 +64,30 @@ def get_total_hours(month, day, filename=None):
     # TODO add flag to convert to work day scale
     # \nOR {(total_hrs)//8} work_days and {total_hrs % 8} hours'
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("echo")
-# args = parser.parse_args()
-# print(args.echo)
-
 
 if __name__ == '__main__':
-    fire.Fire(get_total_hours)
+    parser = argparse.ArgumentParser(
+        prog='daysum',
+        usage='%(prog)s FILENAME\n'
+              '  or:  %(prog)s MONTH DAY')
+
+    parser.add_argument(
+        "file_or_month", help='numeral of month OR name of logfile')
+    parser.add_argument(
+        "day", help='numeral of day IF with month', type=int, nargs='?')
+
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='show additional hourly info')
+
+    # parser.add_argument("--filename", help="explicit logfile name for analysis")
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.file_or_month):
+        # Read an explicitly defined file
+        total_str = get_total_hours(5, 5, filename=args.file_or_month)
+    else:
+        # Default behavior, use month and day to determine filename
+        total_str = get_total_hours(int(args.file_or_month), args.day)
+
+    print(total_str)
