d3779f6e5d150edab47b62b5b481696785a06646
Kellen J. Sam <kellen.sam@bench.com>
2020-12-10 10:39:11 -0700

    Add documentation and linting to util.py

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
+"""Contain general use functions for daysum programs."""
 import datetime as dt
+import sys
 
 # FILENAME = f'log'
 LOG_PATH = '/home/samkel/journal'
@@ -9,19 +11,22 @@ FOLDER_SUFFIX = '_time_sheet'
 
 
 def error_handler(error_str):
+    """Print error info to screen and exit the program."""
     print(error_str)
-    exit(1)
+    sys.exit(1)
+
 
 def beget_path_and_file(year, month, day):
-    """Return full filepath of log file for select date."""
+    """Generate full filepath of log file for select date."""
     date = dt.datetime(year, month, day)
     specific_path = f'{LOG_PATH}/{date.strftime(r"%Y/%b")}{FOLDER_SUFFIX}/'
     filename = f'log{month:02}_{day:02}.txt'
 
     return (specific_path, filename)
 
+
 def beget_filepath(year, month, day):
-    """Return full filepath of log file for select date."""
+    """Generate full filepath of log file for select date."""
     date = dt.datetime(year, month, day)
     specific_path = f'{LOG_PATH}/{date.strftime(r"%Y/%b")}{FOLDER_SUFFIX}/'
     filename = f'log{month:02}_{day:02}.txt'
