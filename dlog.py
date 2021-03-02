6ab4da32349ee4004f5285e7109146475e536edb
Kellen J. Sam <kellonasam@gmail.com>
2021-03-02 10:46:09 -0700

    Add the yester opt arg

--- dlog.py
+++ dlog.py
@@ -28,28 +28,30 @@ def driver():
     parser.add_argument('-v', '--verbose', action='count', default=0,
                         help='show additional hourly info')
 
+    parser.add_argument('-y', '--yester', action='store', default=0,
+                        type=int, nargs='?', const=1,
+                        help="Choose a relative day's date in the past")
+
     args = parser.parse_args()
     today = dt.date.today()
-
-    if args.month is None:
+    if args.yester:
+        days_in_past = dt.timedelta(args.yester)
+        past_date = today - days_in_past
+        logpath, logname = beget_path_and_file(
+            past_date.year, past_date.month, past_date.day)
+    elif args.month is None:
         logpath, logname = beget_path_and_file(
             today.year, today.month, today.day)
-
-        # Create the path to the log file if it does not already exist
-        os.makedirs(logpath, exist_ok=True)
-
-        edit_timesheet(f'{logpath}{logname}')
-
     else:
         # Default behavior, use month and day to determine filename
         args.month = int(args.month)
         logpath, logname = beget_path_and_file(
             today.year, args.month, args.day)
 
-        # Create the path to the log file if it does not already exist
-        os.makedirs(logpath, exist_ok=True)
+    # Create the path to the log file if it does not already exist
+    os.makedirs(logpath, exist_ok=True)
 
-        edit_timesheet(f'{logpath}{logname}')
+    edit_timesheet(f'{logpath}{logname}')
 
 
 if __name__ == '__main__':
